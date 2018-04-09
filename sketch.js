01b67ed03bd89a2051674a976a8ebcbd83982e0e
Lily <yulex.42@gmail.com>
2018-04-09 04:05:01 -0300

    Lots of changes

--- sketch.js
+++ sketch.js
@@ -1,28 +1,29 @@
-const len = 20;
+const w = 20;
+const h = 20;
 
 const container = document.createElement('div');
 container.id = 'container';
 document.body.append(container);
 
-let dots = new Array(len);
+let dots = new Array(w);
 let speeds = [];
 let voff = 0;
 
-for (let i = 0; i < len; i++) {
-  dots[i] = new Array(len);
+for (let i = 0; i < h; i++) {
+  dots[i] = new Array(w);
 
   let row = document.createElement('div');
 
   row.classList.add('row');
   container.append(row);
 
-  for (let j = 0; j < len; j++) {
+  for (let j = 0; j < w; j++) {
     let dot = document.createElement('div');
     dot.classList.add('dot');
     row.append(dot);
     dots[i][j] = dot;
 
-    speeds[i * len + j] = (Math.random() + 1) / 2;
+    speeds[i * w + j] = (Math.random() + 1) / 2;
   }
 }
 
@@ -30,15 +31,15 @@ function draw(start = true) {
   let milli = Date.now();
   let offsum = 0;
 
-  for (let i = 0; i < len; i++) {
-    for (let j = 1; j < len; j++) {
-      let offset = Math.sin(milli * speeds[i * len + j] * 0.003) + 1;
+  for (let i = 0; i < h; i++) {
+    for (let j = 1; j < w; j++) {
+      let offset = Math.sin(milli * speeds[i * w + j] * 0.003) + 1;
       offsum += offset;
       dots[i][j].style.marginLeft = offset + 'vmin';
     }
   }
 
-  let off = offsum/len/len;
+  let off = offsum/w/w;
   voff = start ? off : lerp(voff, off, 0.1);
   document.querySelectorAll('.row + .row')
     .forEach(row => row.style.marginTop = voff + 'vmin');
